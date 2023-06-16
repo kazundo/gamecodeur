ccc853e439ae38902886319ec7afc638f196c4a0
kazundo <ekaniyv@gmail.com>
2023-06-16 08:58:13 +0200

    master

--- Demo/main.lua
+++ Demo/main.lua
@@ -1,3 +1,10 @@
-function love.draw()
-    love.graphics.print("bonjour ceci est mon premier jeux", 400, 300)
-end
+function love.load()
+    love.graphics.setFont(love.graphics.newFont(36))
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setBackgroundColor(0.2, 0.3, 0.4)
+  end
+  
+  function love.draw()
+    love.graphics.printf("Bonjour, ceci est mon premier jeu", 0, 250, love.graphics.getWidth(), "center")
+  end
+  
